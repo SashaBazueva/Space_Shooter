e33e63b79e22f5394bcd96c83cd3d1587cda1d99
Sasha <zvezdopad659527@gmail.com>
2021-05-30 23:22:41 +0500

    картинка движется к месту нажатия, НО! не останавливается

--- core/src/ru/bzbzz/screen/MenuScreen.java
+++ core/src/ru/bzbzz/screen/MenuScreen.java
@@ -12,18 +12,22 @@ public class MenuScreen extends BaseScreen {
     private Texture img;
     private Vector2 pos;
     private Vector2 destination;
+    private Vector2 v;//вектор скорости
+    private Vector2 h;//вектор траектории движения
 
     @Override
     public void show() {
         super.show();
         img = new Texture("frog.jpg");
-        pos = new Vector2(0, 0);
+        pos = new Vector2(0, Gdx.graphics.getHeight());
         destination = new Vector2(0, Gdx.graphics.getHeight());
+        v = new Vector2();
+        h = new Vector2();
     }
 
     @Override
     public void render(float delta) {
-        pos.set(destination);
+        pos.add(v);
         ScreenUtils.clear(0.5f, 0.2f, 0.7f, 1);
         batch.begin();
         batch.draw(img, pos.x, Gdx.graphics.getHeight() - pos.y, 256, 256);
@@ -38,7 +42,12 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        destination.set(screenX - 128, screenY + 128);
+        destination.set(screenX, screenY);
+        h.set(destination.sub(pos));//высчитывание траектории движения
+
+        v.x = h.x > 0 ? 1 : -1;//определение направления движения(скорость движения = 1)
+        v.y = h.y > 0 ? 1 : -1;//
+
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
