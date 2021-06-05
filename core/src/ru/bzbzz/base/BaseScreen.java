b1727579523f369177d09e7d2ba3d43976ff83ed
Sasha <zvezdopad659527@gmail.com>
2021-06-05 11:13:31 +0500

    вернула проект до изменения матрицы

--- core/src/ru/bzbzz/base/BaseScreen.java
+++ core/src/ru/bzbzz/base/BaseScreen.java
@@ -12,7 +12,6 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public void show() {
         batch = new SpriteBatch();
-        batch.getProjectionMatrix().idt();
         Gdx.input.setInputProcessor(this);
     }
 
